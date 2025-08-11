74a0834368a51316e97137ef1a3436477617457a
JomTerry <112268113+JomTerry@users.noreply.github.com>
2025-08-12 03:01:48 +0800

    Update firebase.ts

--- OrganizeMe/src/services/firebase.ts
+++ OrganizeMe/src/services/firebase.ts
@@ -4,13 +4,13 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // TODO: Replace with your own Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyBWawjroPfOoWvUz4VKstv8gn3UYVpLgC4",
-  authDomain: "jomterryy417-c0c.firebaseapp.com",
-  databaseURL: "https://jomterryy417-c0c-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "jomterryy417-c0c",
-  storageBucket: "jomterryy417-c0c.firebasestorage.app",
-  messagingSenderId: "993273611189",
-  appId: "1:993273611189:web:baba2cdc4ff30682904ffc"
+  apiKey: 'AIzaSyBWawjroPfOoWvUz4VKstv8gn3UYVpLgC4',
+  authDomain: 'jomterryy417-c0c.firebaseapp.com',
+  databaseURL: 'https://jomterryy417-c0c-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'jomterryy417-c0c',
+  storageBucket: 'jomterryy417-c0c.firebasestorage.app',
+  messagingSenderId: '993273611189',
+  appId: '1:993273611189:web:baba2cdc4ff30682904ffc'
 };
 
 const app = initializeApp(firebaseConfig);
